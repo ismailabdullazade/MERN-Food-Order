420bdd76a9b777da4afcf937766261d1c90fc9d0
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-02-16 16:55:47 +0400

    auth0 setup

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,7 +1,10 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 
 const MainNav = () => {
+
+  const { loginWithRedirect } = useAuth0()
 
   return (
     <span className="flex space-x-2 items-center">
@@ -9,6 +12,7 @@ const MainNav = () => {
             Order Status
           </Link>
         <Button
+          onClick={async () => await loginWithRedirect()}
           variant="ghost"
           className="font-bold hover:text-orange-500 hover:bg-white"
         >
